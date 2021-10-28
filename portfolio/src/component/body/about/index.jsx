228cccb62cd6b306f9a7b8ebbf660d825b3e5f82
deepak chouhan <blazedboy310@gmail.com>
2021-10-28 20:54:35 +0530

    changing about content

--- portfolio/src/component/body/about/index.jsx
+++ portfolio/src/component/body/about/index.jsx
@@ -9,7 +9,7 @@ function About() {
                 <div className="about_info">
                     Hello there,I am <br/>
                     <span className="info_name">Deepak Chouhan.</span><br/>
-                    I Love experimenting with the web.
+                    I like to Code, Design, Innovate and Experiment. I am an enthusiastic and a social person who loves to take up new challenges and learn new skills. I love meeting new people, exchanging ideas and spreading knowledge and positivity.
                 </div>
                 <div className="about_img">
                     <img src={require("./Coding-bro.png").default}  className="image"/>
