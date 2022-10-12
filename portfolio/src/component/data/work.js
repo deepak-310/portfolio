98845b499631cb42e9f9b638316fb4f09b14f873
Deepak <deepakramchouhan310@gamil.com>
2022-10-12 16:57:52 +0530

    work update

--- portfolio/src/component/data/work.js
+++ portfolio/src/component/data/work.js
@@ -1,12 +1,5 @@
 export const WorkData=[
-    {
-        company:"Dmart",
-        designation:"Sales cashier",
-        doj:"mar 2020",
-        dol:"Dec 2020",
-        companyLogo:require('../../asscits/dmart.png').default,
-        work:"I work as a sales cashier and GRN cashier. I also Work in GRN Section there I need to maintain all files regarding sales and purchase",
-    },
+  
     {
         company:"YIF",
         designation:"Web Development",
